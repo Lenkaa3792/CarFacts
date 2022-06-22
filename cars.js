ec1ea20bb024fe285190c7c97fc6e36d4a047b0c
Lenkaa3792 <allanlenkaa@gmail.com>
2022-06-22 12:50:02 +0300

    updates

--- cars.js
+++ cars.js
@@ -1,13 +1,28 @@
-fetch("https://vpic.nhtsa.dot.gov/api/vehicles/GetMakeForManufacturer/honda")
-  .then((resp) => {
-    return resp.json;
-  })
-    .then( ( data ) => { } );
-  
+// fetch("https://vpic.nhtsa.dot.gov/api/vehicles/GetMakeForManufacturer/honda")
+//   .then((resp) => {
+//     (resp) => resp.json();
+//   })
+//   .then((data) => console.log(data));
 
-const click=document.getElementById('fbtn').addEventListener( 'click ',button1 )
-function button1()
-{
-    alert( 'I am here to know about cars' )
+const click = document
+  .getElementById("fbtn")
+  .addEventListener("click ", button1);
+function button1() {
+  alert("I am here to know about cars");
 }
-    
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "e62126486dmsh39cebd1facfc876p15e1f0jsn32313b0fc1a8",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  },
+};
+
+fetch(
+  "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
+  options
+)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
