03e106e83ba34babf4f677b71c642857b1e2da08
Lenkaa3792 <allanlenkaa@gmail.com>
2022-06-22 23:18:13 +0300

    debugged js file

--- cars.js
+++ cars.js
@@ -19,29 +19,55 @@ const options = {
   },
 };
 
-fetch(
-  "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
-  options
-)
-  .then((response) => response.json())
-    .then( ( data ) =>{
-        console.log( data );
-        let car = data.message;
-        data.forEach( ( data) =>
-        {
-            console.log(data);
-        });
-        
-  })
-    .catch( ( data ) => console.error( data ) );
-    
+// fetch(
+//   "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
+//   options
+// )
+//   .then((response) => response.json())
+//   .then((data) => {
+//     //console.log(data);
+//       let vehicles = data;
+//       let car = data.class;
+//       function Vehicle(vehicles) {
+//         console.log(vehicles);
+//       }
+//       Vehicle();
+//       //console.log(car);
+//     data.forEach((data) => {
+//       console.log(data);
+//     });
+//   })
+//   .catch((data) => console.error(data));
 
-function loadImagesTodom ( car )
+// // function loadImagesTodom(car) {
+// //   console.log(data);
+// //   let dataCount = document.getElementById("fetched");
+// //   let data = document.createElement("li");
+// //   data.innerHTML = data.model;
+// //   dataCount.appendChild(data);
+// // }
+let vehicles = async () =>
 {
-    console.log(data);
-    let carCount = document.getElementById( 'fetched' );
-    let data = document.createElement( 'li' );
-    data.innerHTML = data.model;
-     data.Count.appendChild(corolla);
-}
+    let response = await fetch(
+      "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
+      options
+    );
+    let cars = await response.json()
+    console.log( cars );
     
+  
+
+    let dataCount = document.getElementById( "fetched" );
+      
+    cars.map( ( car ) =>
+    {
+        let row = `<li>model: ${ car.model } make: ${ car.make }</li>`;
+        dataCount.innerHTML+=row
+      console.log(car.model);
+    });
+    // let data = document.createElement("li");
+    // data.innerHTML = `model: ${cars[1].model} make: ${cars[1].make}`;
+    // dataCount.appendChild(data);
+    
+}
+vehicles()
