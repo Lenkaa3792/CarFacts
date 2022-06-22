409020e77c745787406764508c3f77f6a30bf2f1
Lenkaa3792 <allanlenkaa@gmail.com>
2022-06-22 10:54:07 +0300

    updating the js file

--- cars.js
+++ cars.js
@@ -1,6 +1,5 @@
-fetch()
-    .then( resp => resp.json )
-    .then( data =>
-    {
-        
-    })
+fetch("https://api.smartcar.com/v2.0/vehicles")
+    .then( ( resp )      => {
+        return resp.json;
+    } )
+  .then((data) => {});
